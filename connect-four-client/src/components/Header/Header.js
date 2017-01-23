424721e74c61ab212a3c19efa978abbd57474079
Preston Bernstein <Preston Bernstein>
2017-01-23 18:12:40 -0500

    removed unnecessary About route

--- connect-four-client/src/components/Header/Header.js
+++ connect-four-client/src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 
 export const Header = () => (
@@ -9,13 +8,6 @@ export const Header = () => (
       <button className='btn btn-info'>Login</button>
     </div>
     <h1>Connect Four</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/about' activeClassName='route--active'>
-      About
-    </Link>
   </div>
 )
 
--- connect-four-client/tests/components/Header/Header.spec.js
+++ connect-four-client/tests/components/Header/Header.spec.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Header } from 'components/Header/Header'
-import { IndexLink, Link } from 'react-router'
 import { shallow } from 'enzyme'
 
 describe('(Component) Header', () => {
@@ -14,23 +13,5 @@ describe('(Component) Header', () => {
     const welcome = _wrapper.find('h1')
     expect(welcome).to.exist
     expect(welcome.text()).to.match(/Connect Four/)
-  })
-
-  describe('Navigation links...', () => {
-    it('Should render a Link to Home route', () => {
-      expect(_wrapper.contains(
-        <IndexLink activeClassName='route--active' to='/'>
-          Home
-        </IndexLink>
-      )).to.be.true
-    })
-
-    it('Should render a Link to About route', () => {
-      expect(_wrapper.contains(
-        <Link activeClassName='route--active' to='/about'>
-          About
-        </Link>
-      )).to.be.true
-    })
   })
 })
