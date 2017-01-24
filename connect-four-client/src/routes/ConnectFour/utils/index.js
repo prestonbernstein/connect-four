5192cc3b59c1a7eed0a445d611da6bd0113a4642
Preston Bernstein <Preston Bernstein>
2017-01-23 21:51:49 -0500

    add more functions

--- connect-four-client/src/routes/ConnectFour/utils/index.js
+++ connect-four-client/src/routes/ConnectFour/utils/index.js
@@ -6,8 +6,10 @@ export const calculateCurrentPlayer = (previousPlayer) => {
 
 export const calculateBoardUpdate = (board, x, currentPlayer) => {
   const updatedPieceYPosition = calculateNewPieceYPosition(board, x)
+  let lastMove = []
   board[updatedPieceYPosition][x] = currentPlayer
-  return board
+  lastMove = { x: x, y: updatedPieceYPosition }
+  return { board: board, lastMove: lastMove }
 }
 
 const calculateNewPieceYPosition = (board, x) => {
@@ -23,9 +25,13 @@ const calculateNewPieceYPosition = (board, x) => {
   return y
 }
 
-// const findLowestRow = () => {
-//   // iterate through each row to search for first row with pieces currently in it
-//   previousBoard.map(function(i){
-//     return previousBoardRow.every(x => x === 0)
-//   })
-// }
+export const calculateIfGameWon = () => {
+  console.log('calculate if game won')
+  let isGameWon = false
+  return isGameWon
+}
+
+export const calculateAIMove = (lastMove) => {
+  console.log('calculate AI Move')
+  return lastMove
+}
