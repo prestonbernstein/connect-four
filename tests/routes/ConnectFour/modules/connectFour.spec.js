b8842bcaa6970c2d16ab7c5e69efd4617eb5a251
Preston Bernstein <prestonbernstein@gmail.com>
2017-01-25 22:59:48 -0500

    update test

--- tests/routes/ConnectFour/modules/connectFour.spec.js
+++ tests/routes/ConnectFour/modules/connectFour.spec.js
@@ -1,9 +1,37 @@
 import {
-  CONNECT_FOUR_REQUEST_NEW_BOARD
+  CONNECT_FOUR_START_GAME,
+  CONNECT_FOUR_END_GAME,
+  CONNECT_FOUR_UPDATE_BOARD,
+  CONNECT_FOUR_RESET_BOARD,
+  CONNECT_FOUR_CHANGE_CURRENT_PLAYER
 } from 'routes/ConnectFour/modules/connectFour'
 
 describe('(Redux Module) ConnectFour', () => {
-  it('Should export a constant CONNECT_FOUR_REQUEST_NEW_BOARD.', () => {
-    expect(CONNECT_FOUR_REQUEST_NEW_BOARD).to.equal('CONNECT_FOUR_REQUEST_NEW_BOARD')
+  it('Should export a constant CONNECT_FOUR_START_GAME.', () => {
+    expect(CONNECT_FOUR_START_GAME).to.equal('CONNECT_FOUR_START_GAME')
+  })
+})
+
+describe('(Redux Module) ConnectFour', () => {
+  it('Should export a constant CONNECT_FOUR_END_GAME.', () => {
+    expect(CONNECT_FOUR_END_GAME).to.equal('CONNECT_FOUR_END_GAME')
+  })
+})
+
+describe('(Redux Module) ConnectFour', () => {
+  it('Should export a constant CONNECT_FOUR_UPDATE_BOARD.', () => {
+    expect(CONNECT_FOUR_UPDATE_BOARD).to.equal('CONNECT_FOUR_UPDATE_BOARD')
+  })
+})
+
+describe('(Redux Module) ConnectFour', () => {
+  it('Should export a constant CONNECT_FOUR_RESET_BOARD.', () => {
+    expect(CONNECT_FOUR_RESET_BOARD).to.equal('CONNECT_FOUR_RESET_BOARD')
+  })
+})
+
+describe('(Redux Module) ConnectFour', () => {
+  it('Should export a constant CONNECT_FOUR_CHANGE_CURRENT_PLAYER.', () => {
+    expect(CONNECT_FOUR_CHANGE_CURRENT_PLAYER).to.equal('CONNECT_FOUR_CHANGE_CURRENT_PLAYER')
   })
 })
